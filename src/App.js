65b836038180c3adc9091a71d9b5186338d03b98
TaOleks <tahtay.aleksey@gmail.com>
2023-11-30 10:49:09 +0100

    Clining start page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import HelloWorldComponent from './HelloWorldComponent';
 import MyClassComponent from './MyClassComponent';
 function App() {
   const hello = "Hello World"
-  const number = 1
+  const component = 'functional'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           { hello}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {number ===1?<HelloWorldComponent/>: "There i no component found"}
-        <MyClassComponent/>
+        
+        {component ==='functional'?<HelloWorldComponent/>:<MyClassComponent/> }
+        
       </header>
     </div>
   );
